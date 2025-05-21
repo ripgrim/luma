45daceab9fb455ddc5828badd000fe657edd9718
grim <75869731+ripgrim@users.noreply.github.com>
2025-05-20 22:49:25 -0700

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
-import PricingCards from "../components/pricing-cards"
 import { HeroSection } from "@/components/lander/hero-section"
 import { CoolCards } from "@/components/lander/cool-cards"
 import { Features } from "@/components/lander/features"
+import { EarlyAccessProvider } from "@/components/early-access/early-access-provider"
+import { DemoCards } from "@/components/lander/demo-card"
+import { CoolBento } from "@/components/lander/cool-bento"
+import { CoolList } from "@/components/lander/cool-list"
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-[#0c0a09] text-white flex flex-col">
       <Navbar />
-      <HeroSection />
-      <CoolCards />
-      <Features />
+      <EarlyAccessProvider>
+        <HeroSection />
+      </EarlyAccessProvider>  
+      {/* <CoolBento /> */}
+      {/* <CoolList /> */}
+      {/* <DemoCards /> */}
+      {/* <Features /> */}
       <Footer />
     </div>
   )
